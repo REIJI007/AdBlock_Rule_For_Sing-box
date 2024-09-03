4b40261b6e372fa5f71c0692644bb7d8930fc93c
REIJI007 <miyajimareji007@gmail.com>
2024-09-03 19:14:02 +0800

    Update adblock_rule_generator_txt.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_txt.ps1
+++ adblock_rule_generator_txt.ps1
@@ -210,7 +210,7 @@ $($formattedRules -join "`n")
 "@
 
 # 定义输出文件路径
-$outputPath = "$PSScriptRoot/adblock_reject.txt"
+$outputPath = "$PSScriptRoot/adblock_reject_domain.txt"
 $textContent | Out-File -FilePath $outputPath -Encoding utf8
 
 # 输出生成的有效规则总数
